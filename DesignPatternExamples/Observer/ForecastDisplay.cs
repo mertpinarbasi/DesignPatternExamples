8953bd389544f34d39952d566d5be47626da0442
mert.pinarbasi <mert.pinarbasi@piworks.net>
2022-11-08 23:49:41 +0300

    Observer pattern is added

---
+++ DesignPatternExamples/Observer/ForecastDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExamples.Observer
+{
+    class ForecastDisplay : IObserver, IDisplayElement
+    {
+        private float currentPressure = 29.92f;
+        private float lastPressure;
+        private WeatherData weatherData;
+        public void Display()
+        {
+            Console.WriteLine($"Current Pressure {lastPressure}");
+        }
+
+        public ForecastDisplay(WeatherData w)
+        {
+            this.weatherData = w;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update()
+        {
+            lastPressure = currentPressure;
+            currentPressure = weatherData.Pressure;
+            Display();
+        }
+    }
+}
